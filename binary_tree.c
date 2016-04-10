cbaca246198e72149039c5cd564400dcccc2c602
wangliang13 <wangliang13@lenovo.com>
2016-04-10 16:15:49 +0800

    add defination for binary tree

---
+++ binary_tree.c
@@ -0,0 +1,15 @@
+//*************************************************************
+//*
+//* Copyright (c) 2016 : wang liang
+//* License : Distributed under the GNU General Public License
+//* created on : 4/10/2016,  by wang liang (wliked@outlook.com)
+//* 
+//*************************************************************
+
+//node defination of binary tree
+struct BinaryTreeNode 
+{
+	int val;
+	BinaryTreeNode* LNode;
+	BinaryTreeNode* RNode;
+}
