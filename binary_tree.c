6e80408e07f867fa2a901d62a0b23a0d2b5dc219
wangliang13 <wangliang13@lenovo.com>
2016-04-12 23:26:46 +0800

    recusive implement preorder,inorder, postorder traversal of binary tree

--- binary_tree.c
+++ binary_tree.c
@@ -13,3 +13,54 @@ struct BinaryTreeNode
 	BinaryTreeNode* LNode;
 	BinaryTreeNode* RNode;
 }
+ 
+//ouput value of binary tree node 
+void print_btree_node_value(const BinaryTreeNode* btreenode)
+{
+	printf("%d\n",btreenode->val);
+} 
+
+//preorder traversal;implement by recursion 
+void preorder_traversal(const BinaryTreeNode* treeroot)
+{
+	if( NULL==treeroot )
+	{
+	   return;
+	}	
+	else
+	{
+	    print_btree_node_value(treeroot);
+		preorder_traversal(treeroot->LNode);
+        preorder_traversal(treeroot->RNode);		
+	}
+}
+
+//inorder traversal;implement by recursion 
+void inorder_traversal(const BinaryTreeNode* treeroot)
+{
+	if( NULL==treeroot )
+	{
+	   return;
+	}	
+	else
+	{
+		inorder_traversal(treeroot->LNode);
+	    print_btree_node_value(treeroot);
+        inorder_traversal(treeroot->RNode);		
+	}
+}
+
+//postorder traversal;implement by recursion 
+void postorder_traversal(const BinaryTreeNode* treeroot)
+{
+	if( NULL==treeroot )
+	{
+	   return;
+	}	
+	else
+	{
+		postorder_traversal(treeroot->LNode);
+        postorder_traversal(treeroot->RNode);
+        print_btree_node_value(treeroot);		
+	}
+}
